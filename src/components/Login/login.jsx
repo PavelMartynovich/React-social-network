5b619f2c00b03f0a9b54205cfe577fd36f6eb389
Pavel <martynovicpavel3@gmail.com>
2025-07-08 20:23:52 +0300

    react-hook-form, Login form

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,5 +1,69 @@
-import React from "react";
-import {Field, reduxForm} from "redux-form"
+import {useForm} from 'react-hook-form'
+
+const Login= (props) => {
+// useForm() возвращает объект значений!!! Поэтому тут мы и делаем деструктуризирующее присваивание 
+const{register,
+      handleSubmit, 
+      formState:{errors},
+      watch
+   }
+      = useForm() 
+// handleSubmit дает data в onSubmit
+
+const onSubmit = (data) => console.log(data)
+console.log(watch("password")) 
+
+return (
+   /* handleSubmit это контейнерная функция 'react-hook-form' ей мы оборачиваем нашу onSubmit, она
+   делает проверки, и если все хорошо передает значения собранные из input ов в нашу onSubmit, в виде 
+   объекта data
+   */
+   <form onSubmit={handleSubmit(onSubmit)}>
+   <div>
+     <input placeholder='логин' {...register("login", { required: 'введите логин' })} />
+     <div>{errors.login && errors.login.message }</div> 
+   </div>
+     
+   <div>
+     <input placeholder='пароль' {...register("password", { required: 'введите пароль' })} />
+     <div>{errors.password&&errors.password.message}</div>
+     {/* errors will return when field validation fails  */}
+   </div>  
+
+     <input type="checkbox" {...register("checkBox", )} />
+      <div> 
+         <input type="submit" />
+      </div>
+    
+   </form>
+ )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React from "react";
+import { Field, reduxForm } from "redux-form"
 
 const LoginForm = (props) => {
    return (
@@ -8,7 +72,7 @@ const LoginForm = (props) => {
             <Field placeholder={"логин"} name={"login"} component={"input"} />
          </div>
          <div>
-            <Field placeholder={"пароль"} name={"password"} component={"input"}/>
+            <Field placeholder={"пароль"} name={"password"} component={"input"} />
          </div>
          <div>
             <Field type="checkbox" />
@@ -21,23 +85,24 @@ const LoginForm = (props) => {
 }
 
 
-const LoginReduxForm = reduxForm({form:'login'})(LoginForm)
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 
 
 const Login = (props) => {
-   
-   const onSubmit=(banana)=>{
+
+   const onSubmit = (banana) => {
       console.log(banana)
    }
 
    return (
       <div>
          <h1>LOGIN</h1>
-         <LoginReduxForm onSubmit={onSubmit}/>
+         <LoginReduxForm onSubmit={onSubmit} />
       </div>
    )
 
 }
 
+*/
 export default Login
